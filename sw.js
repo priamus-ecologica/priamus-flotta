8d299eb6696bdbf97f0ca786256884cffb9d0a2f
priamus-ecologica <carlo.rossi2005@gmail.com>
2026-08-03 16:55:06 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,36 @@
+// Service worker Priamus - cache dell'interfaccia (non dei dati)
+const CACHE = "priamus-v1";
+const FILES = [
+  "./",
+  "./index.html",
+  "./manifest.json",
+  "./icons/icon-192.png",
+  "./icons/icon-512.png",
+  "./icons/apple-touch-icon.png"
+];
+
+self.addEventListener("install", e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)).then(() => self.skipWaiting()));
+});
+
+self.addEventListener("activate", e => {
+  e.waitUntil(
+    caches.keys().then(k => Promise.all(k.filter(n => n !== CACHE).map(n => caches.delete(n))))
+      .then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener("fetch", e => {
+  const url = e.request.url;
+  // Le chiamate all'API non si mettono mai in cache: i dati devono essere freschi
+  if (url.indexOf("script.google.com") !== -1) return;
+  if (e.request.method !== "GET") return;
+
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request).then(resp => {
+      const copia = resp.clone();
+      caches.open(CACHE).then(c => c.put(e.request, copia)).catch(() => {});
+      return resp;
+    }).catch(() => caches.match("./index.html")))
+  );
+});
