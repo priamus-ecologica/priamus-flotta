05d752b102ae8710ae2ebbcf8c656efa967fe19a
priamus-ecologica <carlo.rossi2005@gmail.com>
2026-08-03 17:16:15 +0200

    Update icon paths in service worker

--- sw.js
+++ sw.js
@@ -4,9 +4,9 @@ const FILES = [
   "./",
   "./index.html",
   "./manifest.json",
-  "./icons/icon-192.png",
-  "./icons/icon-512.png",
-  "./icons/apple-touch-icon.png"
+  "./icon-192.png",
+  "./icon-512.png",
+  "./apple-touch-icon.png"
 ];
 
 self.addEventListener("install", e => {
